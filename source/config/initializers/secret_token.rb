d2fb127dcb7061ea04a15ddc0ba06451de87c8dc
Johnny Thow <johnnyfoe@gmail.com>
2012-05-31 22:06:06 +0100

    Initial commit. Rails project initialised, rpsec added

---
+++ source/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Source::Application.config.secret_token = '8831f6b5d03570ab410dba0b66aabc07bea8f4339f1a8f492ab0d29d0cb0feb3bc875548137caad605852d4d5479d241ad8fcbee7d01c42bd535f3d1f11123b6'
