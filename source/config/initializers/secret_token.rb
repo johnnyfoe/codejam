a679a0dc854b719bca0db9924d5460b8c8d79db2
Johnny Thow <johnnyfoe@gmail.com>
2012-05-31 23:06:01 +0100

    Previous code was using 3.2.1, have updated gemfile etc to use new version 3.2.4

--- source/config/initializers/secret_token.rb
+++ source/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Source::Application.config.secret_token = '8831f6b5d03570ab410dba0b66aabc07bea8f4339f1a8f492ab0d29d0cb0feb3bc875548137caad605852d4d5479d241ad8fcbee7d01c42bd535f3d1f11123b6'
+Source::Application.config.secret_token = '29d90e3c4e4bd1a406f7b0ec5f7b93cb42203146e11f1839d98e81ef6f5b94566b5c5fb54830ef06668446530b366c58e5108721b42f058dfdc80ba449f21b07'
