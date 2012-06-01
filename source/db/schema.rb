191b88b1faa92fb2fdddc6b5521a80a6decfa976
Johnny Thow <johnnyfoe@gmail.com>
2012-06-01 22:27:20 +0100

    added skills and interests

--- source/db/schema.rb
+++ source/db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120601141344) do
+ActiveRecord::Schema.define(:version => 20120601212628) do
+
+  create_table "interests", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "skills", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
